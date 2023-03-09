aaced249e89c06be10592223c7888fe147e076e0
jizhenyu <845590107@qq.com>
2023-03-09 21:46:58 +0800

    fix bugs for version 1.0

--- src/main/java/com/itheima/reggie/config/WebMvcConfig.java
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.example.test.config;
+package com.itheima.reggie.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
@@ -8,14 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Slf4j
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
-
     /**
      * 设置静态资源映射
      * @param registry
-     * */
+     */
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        log.info("开始进行静态资源映射");
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行静态资源映射...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
--- src/test/java/com/itheima/reggie/ReggieApplicationTests.java
+++ src/test/java/com/itheima/reggie/ReggieApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.test;
+package com.itheima.reggie;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestApplicationTests {
+class ReggieApplicationTests {
 
     @Test
     void contextLoads() {
